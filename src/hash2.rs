43d70a0f40c33eb8fb9b808f4411e284f644b5da
atsupontio <akman0510@gmail.com>
2022-12-22 23:56:31 +0900

    a

--- src/hash2.rs
+++ src/hash2.rs
@@ -18,7 +18,10 @@ use ark_crypto_primitives::crh::{
 };
 use ark_ed_on_bls12_381::{constraints::EdwardsVar, EdwardsProjective};
 
-use crate::encode::encode_hex; // import Groth16 library
+use ark_serialize::*;
+use crate::encode_hex;
+
+// use crate::encode::encode_hex; // import Groth16 library
 
 // pub type TwoToOneHash = PedersenCRHCompressor<JubJub, EdwardsVar, Window>;
 
@@ -61,37 +64,55 @@ pub type Image = <TwoToOneHash as TwoToOneCRH>::Output;
 #[allow(clippy::upper_case_acronyms)]
 #[derive(Clone)]
 pub struct HashDemo {
-    pub left: Vec<u8>,
-    pub right: Vec<u8>,
+    pub id: Vec<u8>,
+    pub secret: Vec<u8>,
+    pub nonce: Vec<u8>,
+    pub hashed_name_birth: Vec<u8>,
     pub two_to_one_crh_params: <TwoToOneHash as TwoToOneCRH>::Parameters,
     pub image: Image,
 }
 
 impl ConstraintSynthesizer<Fr> for HashDemo { 
-    fn generate_constraints(self, cs: ConstraintSystemRef<Fr>) -> Result<(), SynthesisError> {
+    fn generate_constraints(mut self, cs: ConstraintSystemRef<Fr>) -> Result<(), SynthesisError> {
 
         let image = ImageVar::new_input(ark_relations::ns!(cs, "image_var"), || Ok(&self.image))?;
 
         let two_to_one_crh_params =
         TwoToOneHashParamsVar::new_constant(ark_relations::ns!(cs, "parameters"), &self.two_to_one_crh_params)?;
 
+        let mut preimage = self.id;
+        let mut secret = self.secret;
+
+        preimage.append(&mut secret);
+        preimage.append(&mut self.nonce);
+        preimage.append(&mut self.hashed_name_birth);
+
+
+        let mut left= Vec::new();
+        let mut right = Vec::new();
+
+        if preimage.len() % 2 == 0 {
+            let len = preimage.len() / 2;
+            right = preimage.split_off(len);
+        } else {
+            preimage.push(000);
+            let len = preimage.len() / 2;
+            right = preimage.split_off(len);
+        }
+        left = preimage;
+
+
         let mut left_bytes = vec![];
-        for byte in self.left.iter() {
+        for byte in left.iter() {
             left_bytes.push(UInt8::new_witness(ark_relations::ns!(cs, "left preimage"), || Ok(byte)).unwrap());
         }
         let mut right_bytes = vec![];
-        for byte in self.right.iter() {
+        for byte in right.iter() {
             right_bytes.push(UInt8::new_witness(ark_relations::ns!(cs, "right preimage"), || Ok(byte)).unwrap());
         }
 
         let hash_result_var = TwoToOneHashGadget::evaluate(&two_to_one_crh_params, &left_bytes, &right_bytes).unwrap();
 
-        // let hash_result = hash_result_var.value().unwrap();
-        // let hash_result = FpVar::<Fr>::new_witness(
-        //     ark_relations::ns!(cs, "new witness x^2"), || Ok(hash_result_var.value())
-        // ).expect("create new witness");
-        // let image = image.value().unwrap();
-        // image.enforce_equal(&hash_result_var.value().unwrap());
         hash_result_var.enforce_equal(&image)?;
 
         Ok(())
@@ -125,6 +146,12 @@ fn test_cube_proof(){
     use crate::encode;
     use crate::encode_hex;
 
+    let mut rng = StdRng::seed_from_u64(0u64);
+
+    //let parameters = TestTwoToOneCRH::setup(rng).unwrap();
+
+    let params = <TwoToOneHash as TwoToOneCRH>::setup(&mut rng).unwrap();
+
     let id = "5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y";
     let secret = "12345678";
     let nonce = "afe387d2";
@@ -134,57 +161,77 @@ fn test_cube_proof(){
     let new_id = base58::FromBase58::from_base58(id).unwrap();
     let new_id2 = encode_hex::encode_hex(new_id.as_slice());
     println!("new_id: {:?}", new_id2);
-    let mut id_bytes = hex::decode(&new_id2).unwrap();
-    println!("id_bytes: {:?}", id_bytes);
+    let id_bytes = hex::decode(&new_id2).unwrap();
+    println!("id_bytes: {:?}", &id_bytes);
 
     let mut secret_bytes = hex::decode(&secret).unwrap();
     println!("secret: {:?}", secret_bytes);
+    let secret_bytes2 = secret_bytes.clone();
 
     let mut nonce_bytes = hex::decode(&nonce).unwrap();
     println!("nonce:{:?}", nonce_bytes);
+    let nonce_bytes2 = nonce_bytes.clone();
 
     let new_name = base58::FromBase58::from_base58(name).unwrap();
     let new_name2 = encode_hex::encode_hex(new_name.as_slice());
-    println!("new_name: {:?}", new_id2);
+    println!("new_name: {:?}", new_name2);
     let mut name_bytes = hex::decode(&new_name2).unwrap();
     println!("name_bytes: {:?}", name_bytes);
 
     let mut birth_bytes = hex::decode(&birth).unwrap();
     println!("secret: {:?}", birth_bytes);
 
-    let mut rng = StdRng::seed_from_u64(0u64);
+    name_bytes.append(&mut birth_bytes);
 
-    //let parameters = TestTwoToOneCRH::setup(rng).unwrap();
+    let mut pub_right = Vec::new();
 
-    let params = <TwoToOneHash as TwoToOneCRH>::setup(&mut rng).unwrap();
+    if name_bytes.len() % 2 == 0 {
+        let len = name_bytes.len() / 2;
+        pub_right = name_bytes.split_off(len);
+    } else {
+        name_bytes.push(000);
+        let len = name_bytes.len() / 2;
+        pub_right = name_bytes.split_off(len);
+    }
 
-    id_bytes.append(&mut secret_bytes);
-    id_bytes.append(&mut nonce_bytes);
-    id_bytes.append(&mut name_bytes);
-    id_bytes.append(&mut birth_bytes);
+
+    let hashed_name_birth = <TwoToOneHash as TwoToOneCRH>::evaluate(&params, &name_bytes.as_slice(), &pub_right.as_slice()).unwrap();
+    let mut result_vec = Vec::new();
+    hashed_name_birth.serialize(&mut result_vec).unwrap();
+    let result2 = result_vec.clone();
+
+    let mut preimage = id_bytes.clone();
+
+    preimage.append(&mut secret_bytes);
+    preimage.append(&mut nonce_bytes);
+    preimage.append(&mut result_vec);
 
     let mut right_half = Vec::new();
 
-    if id_bytes.len() % 2 == 0 {
-        let len = id_bytes.len() / 2;
-        right_half = id_bytes.split_off(len);
+    if preimage.len() % 2 == 0 {
+        let len = preimage.len() / 2;
+        right_half = preimage.split_off(len);
     } else {
-        id_bytes.push(000);
-        let len = id_bytes.len() / 2;
-        right_half = id_bytes.split_off(len);
+        preimage.push(000);
+        let len = preimage.len() / 2;
+        right_half = preimage.split_off(len);
     }
 
-    let left = id_bytes;
+    let left = preimage;
     let right = right_half;
 
     let primitive_result = <TwoToOneHash as TwoToOneCRH>::evaluate(&params, left.as_slice(), right.as_slice()).unwrap();
-    let mut result_vec = Vec::new();
-    primitive_result.serialize(&mut result_vec).unwrap();
-    let hex_result = encode_hex::encode_hex(&result_vec);
+    let mut image_vec = Vec::new();
+    primitive_result.serialize(&mut image_vec).unwrap();
+    let hex_result = encode_hex::encode_hex(&image_vec);
     println!("result: {:?}", hex_result);
+
+
     let circuit = HashDemo {
-        left,
-        right,
+        id: id_bytes,
+        secret: secret_bytes2,
+        nonce: nonce_bytes2,
+        hashed_name_birth: result2,
         two_to_one_crh_params: params,
         image: primitive_result,
     };
